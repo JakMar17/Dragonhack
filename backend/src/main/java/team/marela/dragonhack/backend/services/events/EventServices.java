7562ee290eff05d6c86ddd75c20a0a653b3d832c
JakMar17 <jakob.marusic17@gmail.com>
2022-05-14 19:46:57 +0200

    backend

--- backend/src/main/java/team/marela/dragonhack/backend/services/events/EventServices.java
+++ backend/src/main/java/team/marela/dragonhack/backend/services/events/EventServices.java
@@ -33,8 +33,8 @@ public class EventServices {
                 new HashSet<>(eventDayRepository.saveAll(
                         dto.getDates().stream()
                                 .map(e -> EventDayEntity.builder()
-                                        .eventStart(e.getStartTime())
-                                        .eventEnd(e.getEndTime())
+                                        .eventStart(e.getEventStart())
+                                        .eventEnd(e.getEventEnd())
                                         .event(savedEvent)
                                         .build()
                                 )
