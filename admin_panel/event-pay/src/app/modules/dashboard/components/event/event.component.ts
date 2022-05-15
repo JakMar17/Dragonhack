9c44ae468cdbef3147060c9f0a186b4cef6d1a28
Marko <kofolmarko00@gmail.com>
2022-05-15 10:32:41 +0200

    phnem matjaža :P 👉🏿👈🏿

--- admin_panel/event-pay/src/app/modules/dashboard/components/event/event.component.ts
+++ admin_panel/event-pay/src/app/modules/dashboard/components/event/event.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Event } from '../../classes/event.class';
+import { IDates } from '../../classes/event.class'
 
 @Component({
   selector: 'app-event',
@@ -9,6 +10,9 @@ import { Event } from '../../classes/event.class';
 })
 export class EventComponent implements OnInit {
 
+  public event: Event = new Event({});
+  public subEvents: any[] = [];
+
   constructor(
     protected activatedRoute: ActivatedRoute
   ) { }
@@ -16,9 +20,13 @@ export class EventComponent implements OnInit {
   ngOnInit(): void {
 
     this.activatedRoute.params.subscribe(params => {
-      let id = params['event_id'];
-
-      console.log(id);
+      this.event.endDate = params['endDate'];
+      this.event.eventId = params['eventId'];
+      this.subEvents = params['eventDates'];
+      this.event.eventName = params['eventName'];
+      this.event.image = params['image'];
+      this.event.location = params['location']; 
+      this.event.startDate = params['startDate'];debugger;
     });
 
   }
